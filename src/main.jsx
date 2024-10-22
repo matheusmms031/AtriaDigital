c3218945a5e32e1414a6eb909c1480961ffdca7d
Matheus Magalhães <71942508+matheusmms031@users.noreply.github.com>
2024-10-21 21:48:53 -0300

    Integrate react-router-dom

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import HomeComponent from './pages/HomeComponent.jsx'
-import Home from "./pages/Home.jsx"
+import Home from "./pages/Home/Home.jsx"
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter(
+  [
+    {path:'/',element:<Home/>},
+  ]
+);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Home/>
+    <RouterProvider router={router}/>
   </StrictMode>,
 )
