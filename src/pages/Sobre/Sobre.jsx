ac03a184d1d917ee1281b86b313872288b18d51c
Matheus Magalhães <71942508+matheusmms031@users.noreply.github.com>
2024-10-29 16:30:26 -0300

    Remove a sections in page Sobre

--- src/pages/Sobre/Sobre.jsx
+++ src/pages/Sobre/Sobre.jsx
@@ -2,27 +2,67 @@ import BoxGeneric from "../../components/BoxGeneric/BoxGeneric";
 import styles from "./styles.module.scss";
 import { motion, AnimatePresence } from "framer-motion";
 import { useState } from "react";
-import { Ahistoria, SobreAtria } from "./sections/Sections";
+import video from "../../assets/1015(4).mp4"
 
 export default function Sobre() {
-    const [pageSelect, setPageSelect] = useState("Sobre");
-
-    // Objeto de páginas mapeadas
-    const pages = {
-        "Sobre": SobreAtria,
-        "Historia": Ahistoria
-    };
-
-    // Pegue o componente correto com base na seleção
-    const PageComponent = pages[pageSelect];
 
     return (
         <div className={styles.home}>
             <BoxGeneric>
                 <section>
                     <div className={styles.contentWrapper}>
-                        <ul className={styles.sobreMenu}>
-                            <motion.li
+                        {/* AnimatePresence para animação de saída */}
+                        <div className={styles.pageContent}>
+                            <div className={styles.textContent}>
+                                <div className={styles.boxtext}>
+                                    <motion.h1
+                                        className={styles.mainHeading}
+                                        initial={{ opacity: 0, x: 100 }}
+                                        animate={{ opacity: 1, x: 0 }}
+                                        transition={{
+                                            duration: 1.5,
+                                            type: "spring",
+                                            delay: 0.1,
+                                        }}
+                                    >
+                                        Página de Sobre
+                                    </motion.h1>
+                                    <div className={styles.boxDescriptions}>
+                                        <motion.p
+                                            className={styles.description}
+                                            initial={{ opacity: 0, x: 100 }}
+                                            animate={{ opacity: 1, x: 0 }}
+                                            transition={{
+                                                duration: 1.5,
+                                                type: "spring",
+                                                delay: 0.2,
+                                            }}
+                                        >
+                                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
+                                            tincidunt sagittis laoreet. Morbi tortor lectus, pharetra eget
+                                            fermentum ac, pulvinar eget eros. Nullam at venenatis tellus. Duis
+                                            purus ipsum, elementum eget elementum quis, blandit et ipsum.
+                                            Maecenas sed nisi eget dui euismod accumsan.
+                                        </motion.p>
+                                        <motion.p
+                                            className={styles.description}
+                                            initial={{ opacity: 0, x: 100 }}
+                                            animate={{ opacity: 1, x: 0 }}
+                                            transition={{
+                                                duration: 1.5,
+                                                type: "spring",
+                                                delay: 0.2,
+                                            }}
+                                        >
+                                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
+                                            tincidunt sagittis laoreet. Morbi tortor lectus, pharetra eget
+                                            fermentum ac, pulvinar eget eros. Nullam at venenatis tellus. Duis
+                                            purus ipsum, elementum eget elementum quis, blandit et ipsum.
+                                            Maecenas sed nisi eget dui euismod accumsan.
+                                        </motion.p>
+                                    </div>
+                                </div>
+                                <motion.video src={video} className={styles.video} autoPlay muted loop style={{ width: "30vmax", height: "18vmax", display: "block", position: "relative" }}
                                 initial={{ opacity: 0, x: 100 }}
                                 animate={{ opacity: 1, x: 0 }}
                                 transition={{
@@ -30,53 +70,8 @@ export default function Sobre() {
                                     type: "spring",
                                     delay: 0.5,
                                 }}
-                                onClick={() => setPageSelect("Sobre")}
-                            >
-                                Sobre a Atria
-                            </motion.li>
-                            <motion.li
-                                initial={{ opacity: 0, x: 100 }}
-                                animate={{ opacity: 1, x: 0 }}
-                                transition={{
-                                    duration: 1.5,
-                                    type: "spring",
-                                    delay: 0.8,
-                                }}
-                                onClick={() => setPageSelect("Historia")}
-                            >
-                                A história
-                            </motion.li>
-                            <motion.li
-                                initial={{ opacity: 0, x: 100 }}
-                                animate={{ opacity: 1, x: 0 }}
-                                transition={{
-                                    duration: 1.5,
-                                    type: "spring",
-                                    delay: 1.1,
-                                }}
-                            >
-                                Fazer parte
-                            </motion.li>
-                        </ul>
-
-                        {/* AnimatePresence para animação de saída */}
-                        <div className={styles.pageContent}>
-                            <AnimatePresence mode="wait">
-                                {PageComponent && (
-                                    <motion.div
-                                        key={pageSelect}  // chave única para identificar o componente
-                                        initial={{ opacity: 0, x: 100 }}
-                                        animate={{ opacity: 1, x: 0 }}
-                                        exit={{ opacity: 0, x: -100 }}  // animação de saída
-                                        transition={{
-                                            duration: 0.5,
-                                            type: "spring",
-                                        }}
-                                    >
-                                        <PageComponent />
-                                    </motion.div>
-                                )}
-                            </AnimatePresence>
+                                />
+                            </div>
                         </div>
                     </div>
                 </section>
