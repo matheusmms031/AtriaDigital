7bea6f06ea3474f8fc09add9b932196e9d60a97d
Matheus Magalhães <71942508+matheusmms031@users.noreply.github.com>
2024-10-30 19:09:39 -0300

    Tirar isso né

--- src/pages/Sobre/Sobre.jsx
+++ src/pages/Sobre/Sobre.jsx
@@ -88,7 +88,6 @@ export default function Sobre() {
                                 }}/>
                             </div>
                             <Carousel
-                            images={images}
                             />
                         </div>
                     </div>
